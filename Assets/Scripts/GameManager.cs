da9340e37d375875a4ee2dddb7351a78b82564f9
jborza <jborza@gmail.com>
2017-10-14 15:27:33 +0200

    GameManager added as per the tutorial

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public BoardManager boardScript;
+
+    public static GameManager Instance = null;
+
+    private int level = 1;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
 	}
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
 	
 	// Update is called once per frame
 	void Update () {
