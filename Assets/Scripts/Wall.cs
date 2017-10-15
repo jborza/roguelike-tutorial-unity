1871f78da9b43b251fcab5c07583f3f58d2a7633
jborza <jborza@gmail.com>
2017-10-15 14:40:57 +0200

    Added Wall script

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+	void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+    public void DamageWall(int damage)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp -= damage;
+        if (hp <= 0)
+            gameObject.SetActive(false); //TODO why not destroy?
+    }	
+}
